ebdb9a54e8ccaa529715a96c04f169e478141a74
CompEng0001 <sb1501@canterbury.ac.uk>
2021-10-01 15:21:45 +0100

    <feat> new commands

--- SystemStatistics/systemStat.sh
+++ SystemStatistics/systemStat.sh
@@ -11,7 +11,7 @@ RELEASED="2020-09-16"
 # Display help message
 USAGE(){
          echo -e  $1
-         echo -e "\nUsage: systemStats [-t temperature] [-f arm frequency] [-c cores] [-V volts]"
+         echo -e "\nUsage: systemStats [-t temperature] [-f core frequency] [-c cores] [-V volts]"
          echo -e "\t\t   [-m arm memory] [-M gpu memory] [-f free memory] [-i ipv4 and ipv6 address] "
          echo -e "\t\t   [-v version]"
 	 echo -e "\t\t   more information see: man systemStats"
@@ -30,29 +30,36 @@ elif [[ ( $1 == '--help' ) || ( $1 == '-h' ) ]];then
         exit 1
 fi
 
-# Frequently a script is written so that arguments can be passed in any order  using flags. 
-# With the flags method, some of the arguments can be made optional 
+# Frequently a script is written so that arguments can be passed in any order  using flags.
+# With the flags method, some of the arguments can be made optional
 # a:b means that a mandatory b is not. abc means they all optional
 
-while getopts tfcVmMFiv OPTION
+while getopts tfdDcVmFivu OPTION
 do
 case ${OPTION}
 in
-t) TEMP=$(vcgencmd measure_temp)
+t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
    echo ${TEMP};;
-f) ARMCLOCK=$(vcgencmd measure_clock arm )
-   echo ${ARMCLCOCK};;
+f) COREFREQUENCY=$(cat /sys/devices/system/cpu/cpufreq/policy0/scaling_cur_freq)
+   echo ${COREFREQUENCY};;
 c) CORES=$(cat /sys/devices/system/cpu/present)
    echo "cores="${CORES};;
+d) DISKSPACE=$(df -H | grep -w 'root' | awk '{print $2}')
+   echo "Disk size="${DISKSPACE};;
+D) DISKSPACES=$(df -H  | grep -w 'root' | awk '{print "Total Space: "$2 " Used: "$3 " Avail: "$4}')
+   echo -e "Disk Info="${DISKSPACES};;
 V) VOLT=$(vcgencmd measure_volts core)
    echo ${VOLT};;
-m) ARMMEM=$(vcgencmd get_mem arm)
+u) USAGE=$(grep -w 'cpu ' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)} 
+					    {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)} 
+					    END {printf "Used Cpu: %.2f%%\n",usage}
+                                            {printf "Free Cpu: %.2f%%\n",free}')
+   echo -e ${USAGE};;
+m) ARMMEM=$(cat /proc/meminfo | grep -w 'MemTotal' | awk '{mem=$2/1000} END {printf "%.0fM",mem}' )
    echo ${ARMMEM};;
-M) GPUMEM=$(vcgencmd get_mem gpu)
-   echo ${GPUMEM};;
 F) FREEMEM=$(free -m )
    echo ${FREEMEM};;
-i) IP=$(hostname -I)
+i) IP=$(ifconfig wlan0 | grep -w inet | awk '{ print $2 }')
    echo "IP="${IP};;
 v) echo -e "systemStats:\n\t   Version: ${VERSION}  Released: ${RELEASED} Author: ${AUTHOR}";;
 *) USAGE "\n${*} argument was not recognised";;
