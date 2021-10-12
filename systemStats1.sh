40900d87fdcb9b9b04be976e9191643e55699f1f
CompEng0001 <sb1501@canterbury.ac.uk>
2021-10-12 17:21:06 +0100

    <feat> no returns system cpu usage stats

--- systemStats1.sh
+++ systemStats1.sh
@@ -13,7 +13,7 @@ RELEASED="2021-10-05"
 
 USAGE(){
 	echo -e $1
-	echo -e "\nUsage: systemStats [-t temperature] [-i ipv4 address]"
+	echo -e "\nUsage: systemStats [-t temperature] [-i ipv4 address] [-c cpu usage]"
 	echo -e "\t\t  [-v version]"
 	echo -e "\t\t  more information see man systemStats"
 }
@@ -34,12 +34,17 @@ fi
 # With the flags method, some of the arguments can be made manadatory or optional
 # a:b (a is mandatory, b is optional) abc is all optional
 
-while getopts tiv OPTION
+while getopts ctiv OPTION
 do
 case ${OPTION}
 in
 i) IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2}')
    echo ${IP};;
+c) USAGE=$(grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
+					   {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)} 
+					    END {printf "Used CPU: %.2f%%\n",usage}
+					        {printf "Free CPU: %.2f%%\n",free}')
+   echo -e ${USAGE};;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
    echo ${TEMP} "need to divide by thousand...";;
 v) echo -e "systemStats:\n\t   Version: ${VERSION} Released: ${RELEASED} Author: ${AUTHOR}";;
